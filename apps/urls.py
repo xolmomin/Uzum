74662e69d76f385e0c34c863ed7783201c2396c1
Kholmumin <xolmomin@gmail.com>
2026-02-21 17:24:37 +0500

    add login urls

--- apps/urls.py
+++ apps/urls.py
@@ -4,8 +4,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 # from apps.views import CityListAPIView, DeliveryPointsListAPIView, DeliveryPointsRetrieveAPIView
 
 urlpatterns = [
-    # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # path('cities', CityListAPIView.as_view(), name='city_list_api'),
     # path('delivery-points', DeliveryPointsListAPIView.as_view(), name='delivery_points_api'),
     # path('delivery-points/<int:pk>', DeliveryPointsRetrieveAPIView.as_view(), name='delivery_point_api'),
